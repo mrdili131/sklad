4bc9c4ad29af7a9139261da050f7576fb026bde2
dilmuhammad <dizenstudios@gmail.com>
2026-01-22 23:21:28 +0500

    hello world 2

--- lisa/cart.py
+++ lisa/cart.py
@@ -4,3 +4,17 @@ class Cart:
         self.request = request
         self.session = request.session
         cart = self.session.get("cart")
+
+        if not cart:
+            cart = self.session["cart"] = {}
+        
+        self.cart = cart
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def clear(self):
+        pass
